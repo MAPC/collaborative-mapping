5ec222f06c060ed1fabd3faeaccafc533adaf98a
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-05-13 13:52:18 -0400

    Simplify tooltip form into one onClick event

--- map.js
+++ map.js
@@ -1,9 +1,9 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiaWhpbGwiLCJhIjoiY2plZzUwMTRzMW45NjJxb2R2Z2thOWF1YiJ9.szIAeMS4c9YTgNsJeG36gg';
 const map = new mapboxgl.Map({
-  container: 'map', // container id
-  style: 'mapbox://styles/ihill/cka3y91rj0v1g1inrzaetrs9e/draft', //hosted style id
-  center: [-71.14231, 42.35887], // starting position
-  zoom: 12, // starting zoom
+  container: 'map',
+  style: 'mapbox://styles/ihill/cka3y91rj0v1g1inrzaetrs9e/draft',
+  center: [-71.14231, 42.35887],
+  zoom: 12,
 });
 
 const draw = new MapboxDraw({
@@ -14,48 +14,202 @@ const draw = new MapboxDraw({
     point: true,
     line_string: true,
   },
-  userProperties: true
+  userProperties: true,
+  styles: [
+    // Highlighted point
+    {
+      'id': 'highlight-active-points',
+      'type': 'circle',
+      'filter': ['all',
+        ['==', '$type', 'Point'],
+        ['==', 'meta', 'feature'],
+        ['==', 'active', 'true']],
+      'paint': {
+        'circle-radius': 7,
+        'circle-color': '#000000'
+      }
+    },
+    // Default point
+    {
+      'id': 'points-are-blue',
+      'type': 'circle',
+      'filter': ['all',
+        ['==', '$type', 'Point'],
+        ['==', 'meta', 'feature'],
+        ['==', 'active', 'false']],
+      'paint': {
+        'circle-radius': 5,
+        'circle-color': '#000088'
+      }
+    },
+    // ACTIVE (being drawn)
+    // line stroke
+    {
+        "id": "gl-draw-line",
+        "type": "line",
+        "filter": ["all", ["==", "$type", "LineString"], ["!=", "mode", "static"]],
+        "layout": {
+          "line-cap": "round",
+          "line-join": "round"
+        },
+        "paint": {
+          "line-color": "#D20C0C",
+          "line-dasharray": [0.2, 2],
+          "line-width": 2
+        }
+    },
+    // polygon fill
+    {
+      "id": "gl-draw-polygon-fill",
+      "type": "fill",
+      "filter": ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
+      "paint": {
+        "fill-color": "#D20C0C",
+        "fill-outline-color": "#D20C0C",
+        "fill-opacity": 0.1
+      }
+    },
+    // polygon outline stroke
+    // This doesn't style the first edge of the polygon, which uses the line stroke styling instead
+    {
+      "id": "gl-draw-polygon-stroke-active",
+      "type": "line",
+      "filter": ["all", ["==", "$type", "Polygon"], ["!=", "mode", "static"]],
+      "layout": {
+        "line-cap": "round",
+        "line-join": "round"
+      },
+      "paint": {
+        "line-color": "#D20C0C",
+        "line-dasharray": [0.2, 2],
+        "line-width": 2
+      }
+    },
+    // vertex point halos
+    {
+      "id": "gl-draw-polygon-and-line-vertex-halo-active",
+      "type": "circle",
+      "filter": ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"], ["!=", "mode", "static"]],
+      "paint": {
+        "circle-radius": 5,
+        "circle-color": "#FFF"
+      }
+    },
+    // vertex points
+    {
+      "id": "gl-draw-polygon-and-line-vertex-active",
+      "type": "circle",
+      "filter": ["all", ["==", "meta", "vertex"], ["==", "$type", "Point"], ["!=", "mode", "static"]],
+      "paint": {
+        "circle-radius": 3,
+        "circle-color": "#D20C0C",
+      }
+    },
+
+    // INACTIVE (static, already drawn)
+    // line stroke
+    {
+        "id": "gl-draw-line-static",
+        "type": "line",
+        "filter": ["all", ["==", "$type", "LineString"], ["==", "mode", "static"]],
+        "layout": {
+          "line-cap": "round",
+          "line-join": "round"
+        },
+        "paint": {
+          "line-color": "#000",
+          "line-width": 3
+        }
+    },
+    // polygon fill
+    {
+      "id": "gl-draw-polygon-fill-static",
+      "type": "fill",
+      "filter": ["all", ["==", "$type", "Polygon"], ["==", "mode", "static"]],
+      "paint": {
+        "fill-color": "#000",
+        "fill-outline-color": "#000",
+        "fill-opacity": 0.1
+      }
+    },
+    // polygon outline
+    {
+      "id": "gl-draw-polygon-stroke-static",
+      "type": "line",
+      "filter": ["all", ["==", "$type", "Polygon"], ["==", "mode", "static"]],
+      "layout": {
+        "line-cap": "round",
+        "line-join": "round"
+      },
+      "paint": {
+        "line-color": "#000",
+        "line-width": 3
+      }
+    }
+  ]
 });
 
 map.addControl(draw, 'top-left');
 map.on('load', () => {
-  document.querySelector('#mbta').addEventListener('click', () => {
-    toggleLayer('mbta');
-    toggleLayer('mbta-stops');
-    toggleLayer('bus');
-    toggleLayer('bus-stops');
-    toggleLayer('commuter-rail');
-    toggleLayer('commuter-rail-stops');
-  })
-  map.on('draw.create', function (e) {
-    let coordinates;
-    switch(draw.getMode()) {
-      case 'draw_point':
-        coordinates = e.features[0].geometry.coordinates;
-        break;
-      case 'draw_line_string':
-        coordinates = e.features[0].geometry.coordinates[e.features[0].geometry.coordinates.length - 1];
-        break;
-      case 'draw_polygon':
-        coordinates = e.features[0].geometry.coordinates[0][e.features[0].geometry.coordinates.length - 1];
-        break;
-    }
-    
-    const popup = new mapboxgl.Popup({ closeOnClick: false })
-      .setLngLat(coordinates)
-      .setHTML(`
-        <input type="text" id="myText" placeholder="Title here">
-        <button id="submit">Set</button>`
-      )
-      .addTo(map);
-    popup.getElement().querySelector('#submit').addEventListener('click', () => {
-      setTitle(e.features[0].id, popup);
-    })
-  });
+  map.on('click', function(e) {
+    const selectedFeature = draw.getSelected();
+    if (selectedFeature.features.length > 0) {
+      let coordinates;
+      switch(selectedFeature.features[0].geometry.type) {
+        case 'Point':
+          coordinates = selectedFeature.features[0].geometry.coordinates;
+          break;
+        case 'LineString':
+          coordinates = selectedFeature.features[0].geometry.coordinates[selectedFeature.features[0].geometry.coordinates.length - 1]
+          break;
+        case 'Polygon':
+          coordinates = selectedFeature.features[0].geometry.coordinates[0][selectedFeature.features[0].geometry.coordinates.length - 1];
+          break;
+      }
+      const popup = new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(`
+          <input type="text" id="myText" placeholder="Title here">
+          <button id="submit">Set</button>`
+        )
+        .addTo(map);
+      
+      const currentTitle = selectedFeature.features[0].properties.user__title;
+      if (currentTitle) {
+        document.getElementById("myText").defaultValue = currentTitle
+      }
 
+      popup.getElement().querySelector('#submit').addEventListener('click', () => {
+        setTitle(selectedFeature.features[0].id, popup);
+      })
+    }
+  });
+})
+
+document.querySelector('#mbta').addEventListener('click', () => {
+  toggleLayer('mbta');
+  toggleLayer('mbta-stops');
+  toggleLayer('bus');
+  toggleLayer('bus-stops');
+  toggleLayer('commuter-rail');
+  toggleLayer('commuter-rail-stops');
+})
+saveGeojson();
+
+function toggleLayer(layerId) {
+  const visibility = map.getLayoutProperty(layerId, 'visibility');
+  if (visibility === 'visible' || visibility === undefined) {
+    map.setLayoutProperty(layerId, 'visibility', 'none');
+  } else {
+    map.setLayoutProperty(layerId, 'visibility', 'visible');
+  }
+}
+
+function saveGeojson() {
   const save = document.querySelector('#save')
   save.onclick = function(e) {
     e.preventDefault()
+    console.log(map.getStyle().layers)
     var data = draw.getAll();
     if (data.features.length > 0) {
       const convertedData = 'text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
@@ -66,18 +220,9 @@ map.on('load', () => {
         alert("Wouldn't you like to draw some data?");
     }
   }
-})
+}
 
 function setTitle(featureId, popupObj) {
   draw.setFeatureProperty(featureId,'user__title',document.querySelector('#myText').value);
   popupObj.remove();
-}
-
-function toggleLayer(layerId) {
-  const visibility = map.getLayoutProperty(layerId, 'visibility');
-  if (visibility === 'visible' || visibility === undefined) {
-    map.setLayoutProperty(layerId, 'visibility', 'none');
-  } else {
-    map.setLayoutProperty(layerId, 'visibility', 'visible');
-  }
 }
