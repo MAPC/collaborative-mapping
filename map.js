a39551aa9790db5db5c084dbbbe05b039de6b926
Annabelle Thomas Taylor <ataylor@mapc.org>
2020-05-26 12:40:10 -0400

    Testing functionality - add beforeunload listener

--- map.js
+++ map.js
@@ -363,3 +363,10 @@ function setTitle(featureId, popupObj) {
   draw.setFeatureProperty(featureId, 'user__notes', document.querySelector('#notes').value);
   popupObj.remove();
 }
+
+window.addEventListener('beforeunload', function (e) {
+  // Cancel the event
+  e.preventDefault(); // If you prevent default behavior in Mozilla Firefox prompt will always be shown
+  // Chrome requires returnValue to be set
+  e.returnValue = '';
+});
